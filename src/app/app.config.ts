4f8a56439ea4b2da226fc38d1fdb380e766b7ee3
michel <orestelopez96@gmail.com>
2024-01-22 10:56:33 +0000

    quitando ngxr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,9 +2,7 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideStore } from '@ngrx/store';
-import { provideEffects } from '@ngrx/effects';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideStore(), provideEffects()]
+  providers: [provideRouter(routes), ]
 };
