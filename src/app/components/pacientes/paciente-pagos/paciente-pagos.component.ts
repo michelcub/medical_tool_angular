5c94308c38926ac654e84fbf58e7ab613e5079e4
michel <orestelopez96@gmail.com>
2024-01-28 00:12:41 +0000

    agregado sistema de compro, falta imprementar los servicios con los precios a cobrar y falta tambien agregar la actualizacion del cobro

--- src/app/components/pacientes/paciente-pagos/paciente-pagos.component.ts
+++ src/app/components/pacientes/paciente-pagos/paciente-pagos.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { enviroments } from '../../../../enviroments/enviroments';
+import { RouterModule } from '@angular/router';
+import { ActivatedRoute } from '@angular/router';
 @Component({
   selector: 'app-paciente-pagos',
   templateUrl: './paciente-pagos.component.html',
@@ -8,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PacientePagosComponent implements OnInit {
 
-  constructor() { }
+  cobros:any
+  paciente_id = ''
+  employe_id
 
+  constructor( private route: ActivatedRoute){
+    this.cobros = []
+    this.employe_id = localStorage.getItem('user')
+  }
   ngOnInit() {
+    this.route.params.subscribe(params => {
+      const id = params['id'];  // Replace
+      this.paciente_id = id;
+      this.getPagos()
+    })
   }
 
+
+
+  getPagos(){
+    fetch(enviroments['route-api'] + `/pagos/paciente/${this.paciente_id}`, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': 'Bearer ' + localStorage.getItem('token')
+      },
+    })
+    .then((res) => res.json())
+    .then(data => {
+      console.log('aqui--------- ' + data)
+      this.cobros = data
+    })
+  }
 }
--- src/app/services/episodios/episodio.service.ts
+++ src/app/services/episodios/episodio.service.ts
@@ -21,23 +21,59 @@ export class EpisodioService {
   }
 
   finalizarConsulta() {
-    const episodioActual = this.consultaActualSubject.value; 
+    const episodioActual = this.consultaActualSubject.value;
+    let episodio_id
+    episodioActual?.servicios?.push('consulta') 
     if (episodioActual) {
+
+        //guardar episodio
         fetch(enviroments['route-api'] + '/episodio/new/', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer ' + localStorage.getItem('token')
-            },
-            body: JSON.stringify(episodioActual) // Usa el episodio actual para el cuerpo de la solicitud
-        })
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(error => console.error('Error:', error));
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+              'Authorization': 'Bearer ' + localStorage.getItem('token')
+          },
+          body: JSON.stringify(episodioActual) // Usa el episodio actual para el cuerpo de la solicitud
+          })
+          .then(res => res.json())
+          .then(data => {
+            console.log(data)
+            episodio_id = data._id
+            console.log('episodio_id------ ' + episodio_id)
+            this.generarCobro(episodioActual, episodio_id)
+          })
+          .catch(error => console.error('Error:', error));
+
+        
+
+        
     } else {
         console.log('No hay episodio actual para finalizar');
     }
     this.consultaActualSubject.next(null);
+}
+
+
+generarCobro(episodioActual:Episodio, episodio_id:string):void{
+  //generar cobro
+  fetch(enviroments['route-api'] + '/pagos/',{
+    method: 'POST',
+      headers: {
+          'Content-Type': 'application/json',
+          'Authorization': 'Bearer ' + localStorage.getItem('token')
+      },
+      body: JSON.stringify({
+        "paciente_id": episodioActual.paciente_id,
+        "doctor_id": JSON.parse(episodioActual.employee_id),
+        "monto": 100.00,
+        "cobrado": false,
+        "servicios": episodioActual.servicios,
+        "episodio_id": episodio_id
+    })
+  })
+  .then(res => res.json())
+  .then(data => console.log(data))
+  .catch(error => console.error('Error:', error));
 }
 
 }
